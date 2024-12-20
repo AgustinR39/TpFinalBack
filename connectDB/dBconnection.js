281c3fa24c7ccf629ee1d51b181bbb120feec540
Amanda <mandi.sanchez.97@gmail.com>
2024-12-20 17:26:48 -0300

    Primer commit

---
+++ connectDB/dBconnection.js
@@ -0,0 +1,115 @@
+const mysql= require('mysql2')
+
+const dbConfig={
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'amanda',
+    database: 'tp-final-uade'
+}
+
+const connection =mysql.createConnection(dbConfig)
+
+connection.connect((err)=>{
+    if(err){
+        console.log("Failed to connect")
+        return
+    }
+    console.log("Connection established")
+})
+
+module.exports =connection
+
+
+
+
+
+
+
+
+// const mysql = require('mysql2');
+
+// // Usa MYSQL_URL directamente
+// const connection = mysql.createConnection(process.env.MYSQL_URL);
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Failed to connect to the database:', err.message);
+//     process.exit(1); // Detiene el proceso si no se puede conectar
+//   }
+//   console.log('Connection to the database established');
+// });
+
+// module.exports = connection;
+
+
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT || 3306,
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Failed to connect to the database:', err.message);
+//     process.exit(1);
+//   }
+//   console.log('Connection to the database established');
+// });
+
+// module.exports = connection;
+
+
+// const mysql = require('mysql2');
+
+
+// const dbUrl = process.env.MYSQL_URL;
+
+// // Si la URL de la base de datos no está definida, lanzamos un error.
+// if (!dbUrl) {
+//     console.error('La variable de entorno MYSQL_URL no está definida.');
+//     process.exit(1);
+// }
+
+
+// const connection = mysql.createConnection(dbUrl);
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.log("Failed to connect", err);
+//         return;
+//     }
+//     console.log("Connection established");
+// });
+
+// module.exports = connection;
+
+
+
+
+
+// const mysql= require('mysql2')
+
+// const dbConfig={
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     password: 'amanda',
+//     database: 'tp-final-uade'
+// }
+
+// const connection =mysql.createConnection(dbConfig)
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log("Failed to connect")
+//         return
+//     }
+//     console.log("Connection established")
+// })
+
+// module.exports =connection
---
+++ index.js
@@ -0,0 +1,84 @@
+const express= require('express');
+const app= express();
+const cors = require('cors');
+
+app.use(cors());
+
+//PARA PODER PROCESAR JSON
+
+app.use(express.json());
+
+//LLAMAR A LA RUTAS
+const proveedorroutes= require('./routes/proveedorRoute');
+const clienteroutes= require('./routes/clienteRoute');
+const productoroutes= require('./routes/productoRoute');
+const pedidoroutes= require('./routes/pedidoRoute');
+
+
+app.use('/api/proveedor',proveedorroutes)
+app.use('/uploads', express.static('uploads'));
+app.use('/api/cliente',clienteroutes)
+app.use('/api/producto',productoroutes)
+app.use('/api/pedido',pedidoroutes)
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT,() =>{
+     console.log('listening on port '+PORT);
+ });
+
+
+
+
+
+
+
+// require('dotenv').config();
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// // Configuración de CORS
+// const corsOptions = {
+//   origin: [
+//     'https://final-c7e61.web.app', // URL del nuevo deploy del frontend
+//     'https://final-c7e61.firebaseapp.com', // URL alternativa del nuevo deploy
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
+//   credentials: true, // Permitir cookies y headers personalizados
+// };
+
+// app.use(cors(corsOptions));
+
+// // Middleware para manejar JSON
+// app.use(express.json());
+
+// // Importar rutas
+// const proveedorroutes = require('./routes/proveedorRoute');
+// const clienteroutes = require('./routes/clienteRoute');
+// const productoroutes = require('./routes/productoRoute');
+// const pedidoroutes = require('./routes/pedidoRoute');
+
+// // Configuración de las rutas
+// app.use('/api/proveedor', proveedorroutes);
+// app.use('/uploads', express.static('uploads'));
+// app.use('/api/cliente', clienteroutes);
+// app.use('/api/producto', productoroutes);
+// app.use('/api/pedido', pedidoroutes);
+
+// // Configuración del puerto
+// const PORT = process.env.PORT || 3001;
+
+// // Iniciar el servidor
+// app.listen(PORT, () => {
+//   console.log('Listening on port ' + PORT);
+// });
+
+
+
+
+
+

c59c8207a7b62ebd16fbded1832e3e65480afadf
AgustinR39 <agustinronchi15@gmail.com>
2025-02-07 19:50:22 -0300

    otro intento

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
+app.use((req, res, next) => {
+    console.log(`➡️  Nueva solicitud recibida: ${req.method} ${req.url}`);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('🚀 Backend funcionando correctamente');
 });

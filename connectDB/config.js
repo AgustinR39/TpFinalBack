bea2be8048ede86b16c5e2bb22e73597200c3c6c
AgustinR39 <agustinronchi15@gmail.com>
2025-02-07 15:54:18 -0300

    add module exports en config.js

--- connectDB/config.js
+++ connectDB/config.js
@@ -1,7 +1,16 @@
 export const PORT = process.env.PORT || 3001
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Maitena1.'
-export const DB_NAME = process.env.DB_NAME || 'uade2024'
-export const DB_PORT = process.env.DB_PORT || 3306
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Maitena1.'
+const DB_NAME = process.env.DB_NAME || 'uade2024'
+const DB_PORT = process.env.DB_PORT || 3306
+
+module.exports = {
+    PORT,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+}
